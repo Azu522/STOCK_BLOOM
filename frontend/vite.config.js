b3a32edc1ecd7ec0590ceafd0a83d3561ba82ba9
Karen González <gonsankaazu1814@gmail.com>
2026-06-18 22:17:58 -0600

    Configurar despliegue en GitHub Pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,35 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  base: '/STOCK_BLOOM/',
+  plugins: [
+    react(),
     VitePWA({
-registerType: 'autoUpdate',
-manifest: {
-name: 'STOCK BLOOM',
-short_name: 'SB',
-description: 'Aplicación Web Progresiva con React y Vite',
-theme_color: '#ffffff',
-background_color: '#ffffff',
-display: 'standalone',
-start_url: '/stockbloom',
-icons: [
-{
-src: 'logo2.png',
-sizes: '192x192',
-type: 'image/png'
-},
-{
-src: 'logo2.png',
-sizes: '512x512',
-type: 'image/png'
-}
-]
-}
-})
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'STOCK BLOOM',
+        short_name: 'SB',
+        description: 'Aplicación Web Progresiva con React y Vite',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/STOCK_BLOOM/',
+        icons: [
+          {
+            src: 'logo2.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'logo2.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
-  base: '/stockbloom/'
 })
